0bab9e9e83231acb4b8b2f977b88a6bba4333b7f
Lasse <L.stausgaard@gmail.com>
2020-04-26 13:18:40 +0200

    still playing with conflicts from emrge thanks

--- main.js
+++ main.js
@@ -1,15 +1,23 @@
 "use strict";
 window.addEventListener("DOMContentLoaded", init);
 
-import { test } from "./modules/test";
-import { menuDelegation } from "./modules/menu";
-import { setupForm } from "./modules/form";
-import { get } from "./modules/form";
+import {
+  test
+} from "./modules/test";
+import {
+  menuDelegation
+} from "./modules/menu";
+import {
+  setupForm
+} from "./modules/form";
+import {
+  get
+} from "./modules/form";
 //import { endpoint, apiKey } from "./modules/settings";
 //test(".test h1");
 
 function init() {
-  setupForm();
-  get();
+  /*   setupForm();
+    get(); */
   menuDelegation();
 }
